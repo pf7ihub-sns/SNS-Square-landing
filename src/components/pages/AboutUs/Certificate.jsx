e412f4021c81bc2c0173e0f0636b637bfd26e2a9
Sri_Annaamalai_iHub <annamalai.m.ihub@snsgroups.com>
2025-09-13 13:27:09 +0530

    fix responsive certificate

--- src/components/pages/AboutUs/Certificate.jsx
+++ src/components/pages/AboutUs/Certificate.jsx
@@ -10,23 +10,23 @@ const Certificate = () => {
         style={{ backgroundImage: `url('/images/certificate-BG.png')` }}
       />
       
-      <div className="w-full max-w-8xl mx-auto px-6 sm:px-6 lg:px-45 relative z-10">
-        <div className="grid grid-cols-1 lg:grid-cols-2 items-center bg-white p-8 rounded-3xl">
+      <div className="w-full max-w-8xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
+        <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-y-10 lg:gap-x-16 bg-white p-6 sm:p-8 lg:p-12 rounded-3xl">
           {/* Left Side - Text Content */}
           <div className="text-center lg:text-left">
-            <h2 className="text-3xl sm:text-4xl lg:text-5xl font-manrope font-bold text-global-1 mb-6">
+            <h2 className="text-3xl sm:text-4xl lg:text-5xl font-manrope font-bold text-global-1 mb-4 sm:mb-6">
               <span className="text-[#1E63FF]">Celebrating a Milestone of </span> Excellence.
             </h2>
-            <p className="text-lg sm:text-xl font-inter text-gray-600 leading-relaxed text-center lg:text-left">
+            <p className="text-base sm:text-lg font-inter text-gray-600 leading-relaxed">
               Honoured among the Top 10 Agentic AI Startups of 2025 for redefining the future with innovation.
             </p>
           </div>
 
           {/* Right Side - Certificate Image */}
-          <div className="flex justify-center justify-end">
-            <div className="relative max-w-md w-full">
+          <div className="flex justify-center lg:justify-end">
+            <div className="relative w-full max-w-md">
               <CometCard rotateDepth={15} translateDepth={15}>
-                <div className="bg-[#D5E1FA] rounded-2xl lg:rounded-2xl p-6 lg:p-4 shadow-2xl overflow-hidden cursor-pointer">
+                <div className="bg-[#D5E1FA] rounded-2xl p-4 shadow-2xl overflow-hidden cursor-pointer">
                   <img
                     src="/images/certificate.webp"
                     alt="SNS Square Certificate of Achievement - Top 10 Agentic AI Startups 2025"
