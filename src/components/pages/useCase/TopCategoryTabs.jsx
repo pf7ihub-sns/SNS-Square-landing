952f7769959b16cb8abb439df5ff418f1327c793
kavinbihu <pradeep.s.ihub@snsgroups.com>
2025-09-29 17:01:33 +0530

    ui updated

--- src/components/pages/useCase/TopCategoryTabs.jsx
+++ src/components/pages/useCase/TopCategoryTabs.jsx
@@ -8,7 +8,7 @@ const TopCategoryTabs = ({ categoryFilters, handleCategoryClick }) => {
       <p className="text-center mb-4 md:mb-8 ">
         Explore your business sector
       </p>
-      
+
       {/* Category Tabs */}
       <div className="flex flex-wrap justify-center gap-2 md:gap-4 mb-8 md:mb-12 mt-10">
         {categoryFilters.map((filter) => (
@@ -17,23 +17,17 @@ const TopCategoryTabs = ({ categoryFilters, handleCategoryClick }) => {
             onClick={() => handleCategoryClick(filter.id)}
             variant={filter.isActive ? "black" : "black-outline"}
             size="medium"
-            className="flex items-center gap-2"
+            className="flex items-center gap-2 group"
           >
             <img
-              className="w-5 h-5 md:w-6 md:h-6"
+              className={`w-5 h-5 md:w-6 md:h-6 transition-all duration-200
+                ${
+                  filter.isActive
+                    ? "invert brightness-0 saturate-100" // White when active
+                    : "brightness-95 contrast-95 opacity-70 group-hover:invert-0 group-hover:brightness-95 group-hover:contrast-95"
+                }`}
               alt="Category icon"
               src={filter.icon}
-              style={
-                filter.isActive
-                  ? {
-                      filter: "brightness(0) saturate(100%) invert(100%)",
-                    }
-                  : {
-                      filter:
-                        "brightness(0) saturate(100%) invert(15%) sepia(0%) saturate(0%) hue-rotate(0deg) brightness(95%) contrast(95%)",
-                      opacity: 0.7,
-                    }
-              }
             />
             <span className="font-medium text-sm md:text-base whitespace-nowrap">
               {filter.label}
